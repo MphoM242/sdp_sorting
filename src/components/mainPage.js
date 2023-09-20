bdf4a163c0da3107b1df2be04b1ca25a7ca995ae
mpho <2330688@students.wits.ac.za>
2023-09-20 23:48:26 +0200

    image buttons work

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,17 +1,18 @@
 // src/components/mainPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import pracBut from './pracBut.jpg';
+import pracBut from '../images/pracBut.jpg';
+import testBut from '../images/testBut.jpg';
 
 const MainPage = () => {
   return (
     <div>
       <h1>Welcome to Your Website!!!</h1>
       <Link to="/practice">
-        <img src={pracBut} alt="Practice Button" className="resizedButton" />
+        <img src={pracBut} alt="Practice Mode" className="resizedButton" />
       </Link>
       <Link to="/test">
-        <button>Test Mode</button>
+        <img src={testBut} alt="Test Mode" className="resizedButton" />
       </Link>
     </div>
   );
