3a35b0144bf1db3b70e397ae0514bd37647e88c8
mpho <2330688@students.wits.ac.za>
2023-09-20 22:51:25 +0200

    Trying to add image button 1

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,13 +1,14 @@
 // src/components/mainPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import pracButton from '.public/pracBut.jpg';
 
 const MainPage = () => {
   return (
     <div>
       <h1>Welcome to Your Website!!!</h1>
       <Link to="/practice">
-        <button>Practice</button>
+        <img src={pracButton} alt="Practice Button" />
       </Link>
       <Link to="/test">
         <button>Test Mode</button>
