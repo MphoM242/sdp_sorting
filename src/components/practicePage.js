90da75228541ae1f78223f54666c0a11a274296d
mpho <2330688@students.wits.ac.za>
2023-09-24 18:46:33 +0200

    trying to add  a sidebar

--- src/components/practicePage.js
+++ src/components/practicePage.js
@@ -1,11 +1,24 @@
 // src/components/practicePage.js
 import React from 'react';
+import './PracticePageStyle.css'; 
 
 const PracticePage = () => {
   return (
     <div>
-      <h2>Practice Mode</h2>
-      {/* Add practice content here */}
+      <div className="sidebar">
+        <ul>
+          <li>Bubble Sort</li>
+          <li>MergeSort</li>
+          <li>QuickSort</li>
+          <li>Insertion Sort</li>
+          <li>Selection Sort</li>
+          <li>OtherSort</li>
+        </ul>
+      </div>
+      <div className="content">
+          <h2>Practice Mode</h2>
+          {/* General Sorting info */}
+      </div>
     </div>
   );
 };
