f1b5ef75dd2f60d5bb20167d1f6be79a66e41f56
mpho <2330688@students.wits.ac.za>
2023-09-24 16:49:39 +0200

    content centered finely

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -6,24 +6,27 @@ import testBut from '../images/testBut.jpg';
 
 const MainPage = () => {
   return (
-    <div>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '150vh'}}
+      >
+
       <h1>Data Structures and Algorithms: SORTING</h1>
       <p> Practice mode will allow you to practice sorting algorithms.<br />
           Test mode will allow you to write tests set by your Lecturer.<br />
           <b>NOTE: </b> Feedback may be sent to WITS Moodle page! <br />
       </p>
       <h2>Choose mode:</h2>
-      <Link to="/practice">
-        <img src={pracBut} alt="Practice Mode" className="resizedButton" />
-      </Link>
-      <Link to="/test">
-        <img src={testBut} alt="Test Mode" className="resizedButton" />
-      </Link>
-    
-      <p><font size="-1">SD Project By: <i>"insert cooler group name"</i><br/>
+      <div class="button container">
+        <Link to="/practice">
+          <img src={pracBut} alt="Practice Mode" className="resizedButton" />
+        </Link>
+        <Link to="/test">
+          <img src={testBut} alt="Test Mode" className="resizedButton" />
+        </Link>
+      </div>
+      <br />
+      <p>SD Project By: <i>"insert cooler group name"</i><br/>
       This version is just to get things started and is not the final product (obviously lol).
-      </font></p>
-      
+      </p>
     </div>
     
   );
